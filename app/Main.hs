331a7c8d226587cf2885aea684ef60ff1e6917f4
Oliver Westphal <oliver.westphal@uni-due.de>
2021-08-05 16:04:02 +0200

    simple console app

---
+++ app/Main.hs
@@ -0,0 +1,63 @@
+module Main where
+
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Except (catchError)
+import Control.Exception (displayException)
+
+import Data.List (intercalate)
+
+import System.Console.Haskeline
+import System.Environment (getArgs)
+
+import Paths_prolog (version)
+import Data.Version (showVersion)
+
+import Language.Prolog
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [file] -> runMain $ Just file
+    [] -> runMain Nothing
+    _ -> putStrLn "usage: prolog-interpreter [<file>]"
+
+runMain :: Maybe FilePath -> IO ()
+runMain file = do
+  putStrLn $ unwords ["prolog-interpreter", "version", showVersion version, "(Ctrl-D to exit)"]
+  case file of
+    Nothing -> pure ()
+    Just fname -> do
+      putStrLn $ unwords ["using definitions form", fname]
+      putStrLn "changes to the file do not require reloading"
+  runInputT settings (loop NoTracing)
+  where
+    settings = Settings {
+      complete = completeFilename,
+      historyFile = Just ".prolog-interpreter.history",
+      autoAddHistory = True
+      }
+    loop :: TraceMode -> InputT IO ()
+    loop traceMode = do
+      query <- getInputLine "?- "
+      -- parsing can't handle the . so it is removed (if present)
+      let query' = (\q -> if last q == '.' then init q else q) <$> query
+      case query' of
+        Nothing -> pure () -- EOF / Ctrl-D
+        Just "trace" -> loop Tracing
+        Just "notrace" -> loop NoTracing
+        Just q -> do
+          us <- lift $
+            catchError
+              (runQuery' traceMode file q)
+              -- simple exeption handeling (mainly to avoid crashes)
+              (\e -> putStrLn (displayException e) >> pure [])
+          outputStr $ printUnifiers us
+          loop traceMode
+
+printUnifiers :: [Unifier] -> String
+printUnifiers [] = "false.\n"
+printUnifiers us = unlines [ intercalate ",\n" (map printSubst u) ++ ".\n"| u <- us ]
+  where
+    printSubst :: (VariableName, Term) -> String
+    printSubst (x,t) = unwords [show x, "=", show t]
