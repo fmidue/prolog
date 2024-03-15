2ba8d11a50e8977829f3222411693bc653e68be5
Oliver Westphal <oliver.westphal@uni-due.de>
2024-03-15 17:49:46 +0100

    make benchmark run again

--- bench/Main.hs
+++ bench/Main.hs
@@ -11,15 +11,18 @@ instance NFData Term where
    rnf (Struct a ts) = rnf a `seq` rnf ts
    rnf (Var v)       = rnf v
    rnf (Cut n)       = rnf n
-   rnf Wildcard      = ()
 instance NFData VariableName where
    rnf (VariableName i s) = rnf i `seq` rnf s
+   rnf (Wildcard _)  = ()
 
 
+main :: IO ()
 main = do
-   args <- getArgs
-   let n = case args of { [] -> 6; (x:_) -> read x }
-   Right p <- consult "bench/queens.pl"
-   putStrLn "Starting benchmark..."
-   qs <- resolve p [ts|queens($n,Qs)|]
-   putStrLn $ qs `deepseq` "Number of solutions: " ++ show (length qs)
+  args <- getArgs
+  let n = case args of { [] -> 6; (x:_) -> read x }
+  Right p <- consult "bench/queens.pl"
+  putStrLn "Starting benchmark..."
+  case resolve p [ts|queens($n,Qs)|] of
+    Left err -> putStrLn err
+    Right qs ->
+      putStrLn $ qs `deepseq` "Number of solutions: " ++ show (length qs)
